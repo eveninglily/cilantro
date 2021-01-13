75eebe73918c2c9ea1a2e027f14df655061f2a32
Evan McIntire <mcintire.evan@gmail.com>
2021-01-12 21:48:18 -0800

    CSS Improvements
    Rename some classes for cookbook tiles, and move them into their own component
    
    We also make the tiles look like cards, which I think looks a lot better
    
    Other small changes include
    * Hover effect on sidebar
    * Line separating cookbook metadata and body

--- src/pages/CookbookView.tsx
+++ src/pages/CookbookView.tsx
@@ -1,26 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { CookbookData } from "../models";
+import { CookbookData, Recipe } from "../models";
 
 import "../App.css";
 
-export default function CookbookView(props: CookbookData) {
-  const tiles = props.recipes.map((r) => {
-    const href = "/r/" + r.id;
-    const imgURL = process.env.PUBLIC_URL + "/recipes/images/" + r.imgUrl;
-    return (
-      <div className="cookbook-tile-wrapper">
-        <Link className="cookbook-tile-text" to={href}>
-          {r.title}
-        </Link>
-        <div
-          className="cookbook-tile"
+function CookbookTile(props: Recipe) {
+  const href = "/r/" + props.id;
+  const imgURL = process.env.PUBLIC_URL + "/recipes/images/" + props.imgUrl;
+  const desc = props.bodyText && (props.bodyText.substring(0, 100) + "...");
+
+  return (
+    <div className="cookbook-tile">
+      
+      <div
+          className="cookbook-tile-image"
           style={{
             backgroundImage: `url(${imgURL})`,
           }}
-        ></div>
-      </div>
-    );
+        ><Link className="cookbook-tile-title" to={href}>{props.title}</Link></div>
+        <div className="cookbook-tile-text">
+          {desc}
+        </div>
+    </div>
+  );
+}
+
+export default function CookbookView(props: CookbookData) {
+  const tiles = props.recipes.map((r) => {
+    return CookbookTile(r);
   });
 
   return (
